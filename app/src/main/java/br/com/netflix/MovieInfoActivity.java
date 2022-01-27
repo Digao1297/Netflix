6fcf3f7d6ab4fdef7efe4935688e725fd40a1f9e
digao1297 <digao1297@gmail.com>
2022-01-27 07:43:05 -0300

    :rocket: Implementado activity_movie_info.xml

--- app/src/main/java/br/com/netflix/MovieInfoActivity.java
+++ app/src/main/java/br/com/netflix/MovieInfoActivity.java
@@ -1,15 +1,25 @@
 package br.com.netflix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.netflix.model.Movie;
 
 public class MovieInfoActivity extends AppCompatActivity {
 
@@ -47,5 +57,54 @@ public class MovieInfoActivity extends AppCompatActivity {
         textViewTitle.setText("Batman Begins");
         textViewDesc.setText("O jovem Bruce Wayne viaja para o Extremo Oriente, onde recebe treinamento em artes marciais do mestre Henri Ducard, um membro da misteriosa Liga das Sombras.");
         textViewCast.setText(getString(R.string.cast, ("Christian Bale "+",Michael Caine "+",Liam Neeson")));
+
+        initRecycleView();
+    }
+
+    void initRecycleView(){
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            movies.add(new Movie(R.drawable.movie));
+        }
+        RecyclerView recyclerView = findViewById(R.id.movie_info_recycleview);
+        recyclerView.setAdapter(new MovieSimilarAdapter(movies));
+        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+
+    }
+
+    private static class MovieSimilarHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imageViewCover;
+
+        public MovieSimilarHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewCover = itemView.findViewById(R.id.image_view_cover);
+        }
+    }
+
+    private class MovieSimilarAdapter extends RecyclerView.Adapter<MovieSimilarHolder> {
+
+        private final List<Movie> movies;
+
+        public MovieSimilarAdapter(List<Movie> movies) {
+            this.movies = movies;
+        }
+
+        @NonNull
+        @Override
+        public MovieSimilarHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MovieSimilarHolder(getLayoutInflater().inflate(R.layout.movie_item_similar, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MovieSimilarHolder holder, int position) {
+            Movie movie = movies.get(position);
+//            holder.imageViewCover.setImageResource(movie.getCoverUrl());
+        }
+
+        @Override
+        public int getItemCount() {
+            return movies.size();
+        }
     }
 }
